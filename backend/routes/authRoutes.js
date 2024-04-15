b7462b051a8b6a20c7bd76a3daf37269df3cf8e6
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2024-04-15 20:15:55 +0530

    modifying the code

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controller/authController.js";
-
-const router = express.Router();
-
-router.route("/register").post(register);
-router.route("/login").post(login);
-
-export default router;
--- backend/routes/tagsRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllTags } from "../controller/tagsController.js";
-
-const router = express.Router();
-
-router.route("/").get(getAllTags);
-
-export default router;
