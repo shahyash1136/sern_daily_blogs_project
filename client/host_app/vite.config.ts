4017395c1ba2dbee6032d0ecb48137dabeff6fee
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2024-04-24 22:24:44 +0530

    created initial setup for the UI code

---
+++ client/host_app/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  federation({
+    name: 'host_app',
+    remotes: {},
+    shared: ['react']
+  })],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+
+    minify: false,
+    cssCodeSplit: false,
+  },
+})
